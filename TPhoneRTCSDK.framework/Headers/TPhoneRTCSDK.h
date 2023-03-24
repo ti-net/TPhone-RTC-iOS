93e2b41c510bd906b40877b3f4ab58d5fd39a954
MDIqianxiniao <787602788@qq.com>
2023-03-24 16:32:36 +0800

    重新添加TPhoneRTCSDK

---
+++ TPhoneRTCSDK.framework/Headers/TPhoneRTCSDK.h
@@ -0,0 +1,20 @@
+//
+//  TPhoneRTCSDK.h
+//  TPhoneRTCSDK
+//
+//  Created by 马迪 on 2023/3/14.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TPhoneRTCSDK.
+FOUNDATION_EXPORT double TPhoneRTCSDKVersionNumber;
+
+//! Project version string for TPhoneRTCSDK.
+FOUNDATION_EXPORT const unsigned char TPhoneRTCSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TPhoneRTCSDK/PublicHeader.h>
+
+#import <TPhoneRTCSDK/TPhoneRTCManager.h>
+#import <TPhoneRTCSDK/TPhoneListener.h>
+#import <TPhoneRTCSDK/TLoginModel.h>
